2ca75684562b65d2fe517a35c0a10ab566bdf3b2
User <User@A20>
2021-06-15 15:12:19 +0900

    d

--- java1_1/src/Day05/Day05_2.java
+++ java1_1/src/Day05/Day05_2.java
@@ -121,11 +121,11 @@ public class Day05_2 {
 		// 예제 10 : 입력한 줄 수 만큼 별[*] 출력 [for 중첩]
 			/*예) 5
 			 * 	 *****					i = 1 		s = 1 2 3 4 5 		5-1 => 4+1		5
-			 * 	 ****
-			 * 	 ***
-			 * 	 **
-			 * 	 *
-			 * 
+			 * 	 ****					i = 2		s = 1 2 3 4 		5-2 => 3+1		4			
+			 * 	 ***					i = 3		s = 1 2 3			5-3 => 2+1		3
+			 * 	 **						i = 4 		s = 1 2				5-4 => 1+1		2
+			 * 	 *						i = 5		s = 1				5-5 => 0+1		1
+			 * 							i++			최대줄수-현재줄수 + 1
 			 */
 		
 		int 줄수2 = scan.nextInt();
@@ -133,63 +133,12 @@ public class Day05_2 {
 			for (int j=1 ; j<=줄수2-i ; j++) {
 				System.out.print("*");
 				
-			}
+			}//줄바꿈
 			System.out.println();		
 		}
 		
 		
 		
-		// 예제 11 : for줄수() {for()공백{} for()별{} }
-		int 줄수3 = scan.nextInt();
-		for (int i = 1; i<=줄수3 ; i++) {
-			
-			// 공백이찍기
-			
-			for (int b= 1 ; b<=줄수3-i ; b++)	 {System.out.print(" ");
-			
-			// 별찍기
-			
-			for(int s = 1 ; s<=i; s++) {System.out.println("*");
-			}
-			
-			// 줄바꿈
-		}
-		System.out.println();
 		
-		
-		// 예제 12
-		int 줄수4 = scan.nextInt();
-		
-		for ( int i = 1 ; i<=줄수4 ; i++) {
-			
-			//공백찍기
-			for( int b = 1 ; b<=i-1 ; b++) { System.out.println(" ");}
-			//별찍기
-			for (int s = 1 ; s<=줄수4-i+1 ; s++) {System.out.println("*");}
-			
-			System.out.println();
-		}
-			
-		// 예제 13	
-/*	  1
-	 333
-	55555 	
-   7777777		
-  999999999
- 
- 
- 		*/
-		
-		// 예제 14
-		/*  	  *
-		 *   	 ***
-		 *  	*****
-		 *     *******
-		 *    *********
-		 *    
-		 */
-		
-		// 예제 15
-			
 	}		
 }
