7bd9f2f62f024043b72ee27f00971ac8b19a69dc
kait1987 <85920932+kait1987@users.noreply.github.com>
2021-07-07 16:34:07 +0900

    ...

---
+++ javafx_1/src/application/Main2.java
@@ -0,0 +1,32 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main2 extends Application{
+	
+	public void start(Stage stage) throws Exception{
+		
+		
+		Parent parent = FXMLLoader.load(getClass().getResource("Å×½ºÆ®2.fxml") );
+		
+		
+		
+		Scene scene = new Scene(parent);
+		
+		stage.setScene(scene);
+		
+		stage.show();
+		}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
