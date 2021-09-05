00b2b4e6f11a73f46d73bd4d51ee4582519f32b0
kait1987 <85920932+kait1987@users.noreply.github.com>
2021-09-05 16:55:29 +0900

    ....

--- new/src/new1/Contact.java
+++ new/src/new1/Contact.java
@@ -1,11 +1,8 @@
 package new1;
 
-import java.util.Scanner;
-
 public class Contact {
-	public static void main(String[] args) {
+			
 	
-		
 //		저장정보
 //		이름
 //		전화번호
@@ -13,33 +10,19 @@ public class Contact {
 //		주소 
 //		생일
 //		그룹
+	
 		
-	args = new String[6];
-	args[0] = "이름 : 박주성";
-	args[1] = "전화번호 : 010-0000-0000";
-	args[2] = "이메일 : asdf@asdf.asdf";
-	args[3] = "주소 : 서울";
-	args[4] = "생일 : 2021-09-01";
-	args[5] = "그룹 : 친구";
+	String name = "박주성";
+	String number = "010-0000-0000";
+	String email =  "asdf@asdf.asdf";
+	String address = "서울";
+	String brithday = "2021-09-01";
+	String group = "친구";
 	
-	System.out.println(args[0]);
-	System.out.println(args[1]);
-	System.out.println(args[2]);
-	System.out.println(args[3]);
-	System.out.println(args[4]);
-	System.out.println(args[5]);
-	
-	
-	System.out.println("----------------------");
-	System.out.println("그룸정보변경");
-	System.out.println("----------------------");
-	System.out.println("이름 : 박주성");
-	System.out.println("전화번호 : 010-0000-0000");
-	System.out.println("에미일 : asdf@asdf.asdf");
-	System.out.println("주소 : 서울 ");
-	System.out.println("생일 : 2021-09-01");
-	System.out.println("그룹 : 가족");
-	System.out.println("---------------------");
-	}
 
+	
+	
+	
+	
+	
 }
--- new/src/new1/ContactMain.java
+++ new/src/new1/ContactMain.java
@@ -1,12 +1,24 @@
 package new1;
 
-import new1.Contact;
-
 public class ContactMain {
 	public static void main(String[] args) {
-		Contact contact = new Contact();
-		String i = contact.a("이름:");
-		System.out.println(i);
+		
+
+		
+	Contact contact = new Contact();
+	
+	System.out.println("이름 :" + contact.name);
+	System.out.println("전화번호 :" + contact.number);
+	System.out.println("이메일" + contact.email);
+	System.out.println("주소 "+ contact.address);
+	System.out.println("그룹" + contact.group);
+		
+	
+//	필드값 변경
+//	친구 > 가족
+		
+	contact.group = "가족";
+	System.out.println("수정된 그룹" + contact.group);
 	}
 
 }
