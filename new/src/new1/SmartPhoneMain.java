0fdfc57db36181ac5a4ea8c4ddef1d915b142129
me <lenigi2000@gmail.com>
2021-09-06 02:34:16 +0900

    ..

--- new/src/new1/SmartPhoneMain.java
+++ new/src/new1/SmartPhoneMain.java
@@ -1,13 +1,5 @@
 package new1;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class SmartPhoneMain {
-	Scanner scan = new Scanner(System.in);
-	
-	public static void main(String[] args) {
-		private  =  
-	}
+
 }
