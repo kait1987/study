aea8f2b0d02509232e37fec0de33f91ba8343f76
me <lenigi2000@gmail.com>
2021-09-06 11:33:55 +0900

    ....

--- new/src/new1/SmartPhone.java
+++ new/src/new1/SmartPhone.java
@@ -2,32 +2,27 @@ package new1;
 
 public class SmartPhone {
 		
-	private String name;
-	private String number;
-	private String email;
-	private String address;
-	private String birthday;
-	private String group;
+	String name;
+	String number;
+	String email;
+	String address;
+	String birthday;
+	String group;
+	
+	public SmartPhone(String name, String number, String email, String address, String birthday, String group) {
+		this.name = name;
+		this.number = number;
+		this.email = email;
+		this.address = address;
+		this.birthday = birthday;
+		this.group = group;
+	}
 	
 	
-	public String getName() {
-		return name;
-	}
-	public String getNumber() {
-		return number;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public String getBirthday() {
-		return birthday;
-	}
-	public String getGroup() {
-		return group;
-	}
+	
+	
+	
+	
 	
 	
 	
--- new/src/new1/SmartPhoneMain.java
+++ new/src/new1/SmartPhoneMain.java
@@ -1,5 +1,12 @@
 package new1;
 
+import java.util.Scanner;
+
 public class SmartPhoneMain {
+	Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+	}
 
 }
