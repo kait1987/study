3cc94ab4b395f3be660c9a89352f08e1a2b92fca
kait1987 <85920932+kait1987@users.noreply.github.com>
2021-09-13 00:40:44 +0900

    ...

---
+++ project/src/Project05/Person.java
@@ -0,0 +1,11 @@
+package Project05;
+
+public class Person {
+	
+	// 다음 클래스를 다이어 그램을 코드로 작성
+	
+	// person --------> -phones(변수), 2(변수) / phone (연과관계)
+	public static void main(String[] args) {
+		
+	}
+}
